d85962f23f59017e7eb046209d0a5757037bf4c0
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2021-08-24 17:28:24 +0100

    Added DD gradient function trfun2ddgrad

--- utils/trfun2dd.m
+++ utils/trfun2dd.m
@@ -13,4 +13,4 @@ for k = 1:h % over [0,pi]
 	LHk = LT*H(:,:,k);
 	d(k) = sum(log(diag(chol(LHk*LHk')))); % (log-determinant)/2
 end
-D = sum((d(1:end-1)+d(2:end)))/(h-1); % integrate frequency-domain DD (trapezoidal rule) to get time-domain DD
+D = sum(d(1:end-1)+d(2:end))/(h-1); % integrate frequency-domain DD (trapezoidal rule) to get time-domain DD
---
+++ utils/trfun2ddgrad.m
@@ -0,0 +1,18 @@
+function [G,g] = trfun2ddgrad(L,H)
+
+% Calculate gradient of spectral dynamical dependence
+% of projection L from transfer function H.
+%
+% NOTE 1: assumes identity residuals covariance matrix
+% NOTE 2: projection L MUST be orthonormal!!!
+
+h = size(H,3);
+[n,m] = size(L);
+g = zeros(m,n,h);
+LT = L';
+for k = 1:h % over [0,pi]
+	Hk  = H(:,:,k);
+	LHk = LT*Hk;
+	g(:,:,k) = real((LHk*LHk')\LHk*Hk'); % grad/2
+end
+G = sum(g(:,:,1:end-1)+g(:,:,2:end),3)/(h-1); % integrate frequency-domain gradient (trapezoidal rule) to get time-domain gradient
