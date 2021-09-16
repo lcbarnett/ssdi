cae5b5191f6462479f8d0e6d5ec306e9287442ee
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2021-09-16 16:17:21 +0100

    Added gradient descent (spectral) DD optimisation

---
+++ utils/opt_es_gd_dds.m
@@ -0,0 +1,76 @@
+function [dopt,Lopt,converged,sig,iters,dhist] = opt_es_gd_dds(H,Lopt,maxiters,sig,ifac,nfac,tol,hist)
+
+% Assumptions
+%
+% 1 - Lopt is orthonormal
+% 2 - Residuals covariance matrix is identity
+
+if isscalar(tol)
+	stol = tol;
+	dtol = tol;
+else
+	stol = tol(1);
+	dtol = tol(2);
+end
+
+[n,m] = size(Lopt);
+
+% Calculate dynamical dependence of initial projection
+
+dopt = trfun2dd(Lopt,H);
+
+if hist
+	dhist = zeros(maxiters,3);
+	i = 1; dhist(i,:) = [1 dopt sig];
+else
+	dhist = [];
+end
+
+% Optimise
+
+converged = 0;
+for iters = 2:maxiters
+
+	% Move down gradient and orthonormalise
+
+	Ltry = orthonormalise(Lopt - sig*trfun2ddgrad(L,H));
+
+	% Calculate dynamical dependence of candidate projection
+
+	dtry = trfun2dd(Ltry,H);
+
+	% If dynamical dependence smaller, accept mutant
+
+	if dtry < dopt
+		Lopt = Ltry;
+		if hist
+			i = i+1; dhist(i,:) = [iters dopt sig];
+			dopt = dtry;
+			sig  = ifac*sig;
+			i = i+1; dhist(i,:) = [iters dopt sig];
+		else
+			dopt = dtry;
+			sig  = ifac*sig;
+		end
+	else
+		sig = nfac*sig;
+	end
+
+	% Test convergence
+
+	if sig < stol
+		converged = 1;
+		break
+	end
+
+	if dopt < dtol
+		converged = 2;
+		break
+	end
+
+end
+
+if hist
+	i = i+1; dhist(i,:) = [iters dopt sig];
+	dhist = dhist(1:i,:);
+end
