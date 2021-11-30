fc537fe99d30c8fd6949c3f9895bb9ea0bc4fcf5
lcbarnett <l.c.barnett@sussex.ac.uk>
2021-11-30 10:33:54 +0000

    Correction in DD gradient function

--- experimental/trfun2ddgrad.m
+++ experimental/trfun2ddgrad.m
@@ -1,4 +1,4 @@
-function [G,g] = trfun2ddgrad(L,H)
+function G = trfun2ddgrad(L,H)
 
 % Calculate gradient of spectral dynamical dependence
 % of projection L from transfer function H.
@@ -16,3 +16,5 @@ for k = 1:h % over [0,pi]
 	g(:,:,k) = real((LHk*LHk')\LHk*Hk'); % grad/2
 end
 G = sum(g(:,:,1:end-1)+g(:,:,2:end),3)'/(h-1); % integrate frequency-domain gradient (trapezoidal rule) to get time-domain gradient
+
+G = G-(L*L')*G; % Edelman-Arias-Smith
