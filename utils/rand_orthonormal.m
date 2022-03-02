4dfbc886b762ad931c4713d93a9de216533fb230
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2022-03-02 11:59:37 +0000

    Default 3rd parameter for rand_orthonormal

--- utils/rand_orthonormal.m
+++ utils/rand_orthonormal.m
@@ -3,6 +3,8 @@ function [L,M] = rand_orthonormal(n,m,r)
 % Returns r random n x m orthonormal matrices, and
 % optionally their n x (n-m) orthogonal complements.
 
+if nargin < 3 || isempty(r), r = 1; end
+
 L = randn(n,m,r);
 if nargout > 1
 	M = zeros(n,m,r);
